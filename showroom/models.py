6d3c1a6e39cf686485bcb5a233f508918c61d390
Anton Kalesnikovich <ant.kolesnikovich@gmail.com>
2023-12-18 14:16:13 +0300

    Fixed pull request 1 comments

--- showroom/models.py
+++ showroom/models.py
@@ -1,46 +1,43 @@
 from django.db import models
 from django.db.models.functions import Now
 from django_countries.fields import CountryField
+from django.contrib.postgres.fields import ArrayField
 
 
-class Showroom(models.Model):
+class BaseModel(models.Model):
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(db_default=Now())
+
+
+class Showroom(BaseModel):
     name = models.CharField(max_length=50)
     country = CountryField()
-    is_active = models.BooleanField(default=True)
     balance = models.DecimalField(max_digits=15, decimal_places=2)
-    created_at = models.DateTimeField(db_default=Now())
     max_car_year = models.IntegerField(max_length=4, null=True)
     min_car_year = models.IntegerField(max_length=4, null=True)
+    allowed_brands = ArrayField(models.CharField(max_length=30), null=True)
+    allowed_countries = ArrayField(CountryField(), null=True)
 
 
-class Car(models.Model):
+class Car(BaseModel):
+    production_brand = models.CharField(max_length=20)
     name = models.CharField()
     production_country = CountryField()
-    is_active = models.BooleanField(default=True)
     production_year = models.IntegerField(max_length=4)
+
+
+class CarShowroom(BaseModel):
+    car = models.ForeignKey(Car, on_delete=models.PROTECT)
     showroom = models.ForeignKey(Showroom, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     color = models.CharField(max_length=20)
+    mileage = models.DecimalField(max_digits=8, decimal_places=1)
 
 
-class Discount(models.Model):
+class Discount(BaseModel):
     percentage = models.DecimalField(max_digits=3, decimal_places=1)
     name = models.CharField(max_length=50)
     description = models.TextField()
     showroom = models.ForeignKey(Showroom, on_delete=models.CASCADE)
     cars = models.ManyToManyField(Car)
-    is_active = models.BooleanField(default=True)
-
-
-class AllowedCarBrand(models.Model):
-    name = models.CharField(max_length=30)
-    showroom = models.ForeignKey(Showroom, on_delete=models.CASCADE)
-
-
-class AllowedCarCountry(models.Model):
-    country = CountryField()
-    showroom = models.ForeignKey(Showroom, on_delete=models.CASCADE)
-
-
-
-
 
