a992bf1a1a413f46f6cc0df0c61636aa7f374882
Anton Kalesnikovich <ant.kolesnikovich@gmail.com>
2023-12-18 17:08:18 +0300

    Create basic Customer model

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from showroom.models import BaseModel, Car, Showroom
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Customer(BaseModel):
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    balance = models.DecimalField(max_digits=15, decimal_places=2)
+    email = models.EmailField()
+    username = models.CharField(max_length=30)
+
+
+class PurchaseOffer(BaseModel):
+
+    client = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    max_price = models.DecimalField(max_digits=15, decimal_places=2)
+    color = models.CharField(max_length=20, blank=True)
+
+
+class Purchase(BaseModel):
+
+    client = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.PROTECT)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    showroom = models.ForeignKey(Showroom, on_delete=models.PROTECT)
