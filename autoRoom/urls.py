a7e458e7c704fc40a0b792432db956ba6297a36d
Anton Kalesnikovich <ant.kolesnikovich@gmail.com>
2023-12-29 17:20:50 +0300

    Added jwt auth

--- autoRoom/urls.py
+++ autoRoom/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
 urlpatterns = [
@@ -23,4 +24,7 @@ urlpatterns = [
     path("", include("showroom.urls")),
     path("", include("customer.urls")),
     path("", include("supplier.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
